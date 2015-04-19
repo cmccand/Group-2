2844dfc62e6bec0977493eef0a001176ac77b55b
Chris McCandlish <cmccand@gmail.com>
2015-04-19 16:15:58 -0400

    making more responsive

--- client/js/template.js
+++ client/js/template.js
@@ -4,8 +4,8 @@ var templates = {}
 
     "<h3><%= word_one %></h3>",
     "<h3><%= word_two %></h3>",
-    "<h4><%= shoutout %></h4>",
-    "<p><%= comments %></p>",
+    // "<h4><%= shoutout %></h4>",
+    // "<p><%= comments %></p>",
     "<img src= <%= photo %> />"
 
 
